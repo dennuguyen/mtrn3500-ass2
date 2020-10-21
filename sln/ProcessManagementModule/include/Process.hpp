fd5470f8c9f664e437eb9d9090a9279d994d3986
dennuguyen <dennuguyen@gmail.com>
2020-10-22 00:53:23 +1100

    Open file map in Processes

--- sln/ProcessManagementModule/include/Process.hpp
+++ sln/ProcessManagementModule/include/Process.hpp
@@ -4,13 +4,23 @@
 
 struct Process {
    public:
-	Process(LPWSTR path);
+    Process(LPWSTR path);
 	~Process();
 
+	void restart();
+	void openFileMapping(std::wstring name);
+	void* mappedViewAddr(int size);
 	bool isAlive();
+	void kill();
+
+	friend std::wostream& operator<<(std::wostream& output, Process& process);
 
    public:
-	LPWSTR path;
-	STARTUPINFO sinfo;
-	PROCESS_INFORMATION pinfo;
+	LPWSTR path;                // path to process executable
+	STARTUPINFO sinfo;          // startup info
+	PROCESS_INFORMATION pinfo;  // process info
+	HANDLE handle;              // unique handle to file mapping object
+
+   private:
+	void* memptr;  // pointer in map view
 };
--- sln/ProcessManagementModule/src/Process.cpp
+++ sln/ProcessManagementModule/src/Process.cpp
@@ -3,13 +3,13 @@
 
 #include "Process.hpp"
 
-Process::Process(LPWSTR path) : path(path), sinfo({}), pinfo({}) {
+Process::Process(LPWSTR path) : path(path), sinfo({}), pinfo({}), handle() {
     if (CreateProcess(NULL,
 		              (LPWSTR)std::wstring(path).c_str(),
 		              NULL,
 		              NULL,
 		              FALSE,
-		              CREATE_NEW_CONSOLE | CREATE_NEW_PROCESS_GROUP,
+		              CREATE_NEW_CONSOLE,
 		              NULL,
 		              NULL,
 		              &sinfo,
@@ -19,8 +19,30 @@ Process::Process(LPWSTR path) : path(path), sinfo({}), pinfo({}) {
 	}
 }
 
-Process::~Process() {
-	CloseHandle(pinfo.hProcess);
+Process::~Process() {}
+
+void Process::restart() {
+	HANDLE process = OpenProcess(SYNCHRONIZE, FALSE, pinfo.dwProcessId);
+}
+
+void Process::openFileMapping(std::wstring name) {
+	handle = OpenFileMapping(FILE_MAP_ALL_ACCESS,
+		                     FALSE,
+		                     name.c_str());
+
+	if (handle == INVALID_HANDLE_VALUE) {
+		std::cerr << "ERROR " << INVALID_HANDLE_VALUE << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void* Process::mappedViewAddr(int size) {
+	memptr = MapViewOfFile(handle,
+		                   FILE_MAP_ALL_ACCESS,
+		                   0,
+		                   0,
+		                   size);
+	return memptr;
 }
 
 bool Process::isAlive() {
@@ -28,4 +50,14 @@ bool Process::isAlive() {
 	DWORD status = WaitForSingleObject(process, 0);
 	CloseHandle(process);
 	return status == WAIT_TIMEOUT;
+}
+
+void Process::kill() {
+	CloseHandle(handle);
+	TerminateProcess(pinfo.hProcess, EXIT_SUCCESS);
+}
+
+std::wostream& operator<<(std::wostream& output, Process& process) {
+	output << process.path;
+	return output;
 }
