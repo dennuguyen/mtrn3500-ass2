813cfa5318bac77a14e21fe21da0093be7ed6c0c
dennuguyen <dennuguyen@gmail.com>
2020-11-05 12:19:18 +1100

    Create FMO and TCP client

--- sln/GNSSModule/src/Main.cpp
+++ sln/GNSSModule/src/Main.cpp
@@ -1,3 +1,21 @@
+#include "Modules.hpp"
+#include "SharedMemory.hpp"
+#include "TCPClient.hpp"
+
 int main(int argc, char** argv) {
-	return 0;
+
+    // Create file mapping object for this process
+    sm::FileMappingObject map(mod::GPS.name, sm::SIZE);
+    map.openFileMapping();
+    map.mappedViewAddr();
+
+    // Create TCP client
+    tcp::TCPClient client(mod::GPS.ip, mod::GPS.port, mod::ZID);
+    client.tcpConnect();
+
+    while (1) {
+
+    }
+
+	return EXIT_SUCCESS;
 }
