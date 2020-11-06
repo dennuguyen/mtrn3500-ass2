823f05776a98d0b4755690d1c5fb3587bb2c7265
dennuguyen <dennuguyen@gmail.com>
2020-11-06 20:49:10 +1100

    Turn on optimisations

--- sln/include/Modules.hpp
+++ sln/include/Modules.hpp
@@ -1,24 +1,35 @@
 #ifndef MODULES_HPP_
 #define MODULES_HPP_
 
+#include <vector>
+
 namespace mod {
 
 const std::string ZID = "z5206032\n";
 
-struct Module {
+/**
+ * Module struct to hold important module information
+ */
+struct ModuleInfo {
     const std::wstring name;
     const std::wstring ip;
     const int port;
+    const int heartbeat;
 };
 
-const Module CAMERA = { L"CameraModule.exe", L"192.168.1.200", 26000 };
-const Module DISPLAY = { L"DisplayModule.exe" };
-const Module GPS = { L"GNSSModule.exe", L"192.168.1.200", 24000 };
-const Module LASER = { L"LaserModule.exe", L"192.168.1.200", 23000 };
-const Module MANAGE = { L"ProcessManagementModule.exe" };
-const Module TELEOP = { L"VehicleControlModule.exe", L"192.168.1.200", 25000 };
 
-const Module MODULES[] = {
+/* Constant module definitions */
+const ModuleInfo CAMERA = { L"CameraModule.exe", L"192.168.1.200", 26000, 0 };
+const ModuleInfo DISPLAY = { L"DisplayModule.exe", L"", -1, 1 };
+const ModuleInfo GPS = { L"GNSSModule.exe", L"192.168.1.200", 24000, 2 };
+const ModuleInfo LASER = { L"LaserModule.exe", L"192.168.1.200", 23000, 3 };
+const ModuleInfo MANAGE = { L"ProcessManagementModule.exe", L"", -1, 4 };
+const ModuleInfo TELEOP = { L"VehicleControlModule.exe", L"192.168.1.200", 25000, 5 };
+
+/**
+ * Modules to startup when process management starts
+ */
+const ModuleInfo STARTUP[] = {
     DISPLAY,
     LASER,
     GPS,
