a0a7e1f08861a4ab670bc0b311ed31dce829932e
Dan Nguyen <z5206032@ad.unsw.edu.au>
2020-11-08 04:13:28 +1100

    Fix bug for wrong FMO name used

--- sln/VehicleControlModule/src/Main.cpp
+++ sln/VehicleControlModule/src/Main.cpp
@@ -1,6 +1,7 @@
 #include <Winsock2.h>
 
 #include <iostream>
+#include <sstream>
 
 #include "Modules.hpp"
 #include "SharedMemory.hpp"
@@ -14,7 +15,7 @@ int main(int argc, char** argv) {
     map.mappedViewAddr();
 
     // Create file mapping object to process management
-    sm::FileMappingObject management(mod::TELEOP.name, sm::SIZE);
+    sm::FileMappingObject management(mod::MANAGE.name, sm::SIZE);
     management.openFileMapping();
     bool* heartbeat = (bool*)((char*)management.mappedViewAddr() + mod::TELEOP.heartbeat);
 
@@ -28,15 +29,29 @@ int main(int argc, char** argv) {
 
     while (!timer.expired()) {
         if (*heartbeat == false) {
+            
+            // Send teleop
+            std::stringstream command;
+            command << "# 20 0.5 1 #";
+            std::cout << client.tcpSend(command.str()) << std::endl;
+
+            // Give server time to prepare data
+            Sleep(500);
+            /*
+            // Confirm command
+            std::string buffer = client.tcpReceive();
+            std::cout << buffer << std::endl;
+            */
             // Set heartbeat
             *heartbeat = true;
             timer.time(tmr::TIMEOUT_4S);
         }
 
-        Sleep(1000);  // 1000 ms refresh rate
+        Sleep(100);  // 100 ms refresh rate
     }
 
     client.tcpClose();
 
     return EXIT_SUCCESS;
 }
+
