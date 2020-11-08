9462deb374b6d249b912d342e0896d486d0ff86b
Dan Nguyen <z5206032@ad.unsw.edu.au>
2020-11-08 13:47:36 +1100

    Validate CRC value

--- sln/GNSSModule/src/Main.cpp
+++ sln/GNSSModule/src/Main.cpp
@@ -7,46 +7,17 @@
 #include "SharedMemory.hpp"
 #include "TCPClient.hpp"  // #include <Winsock2.h>
 #include "Timer.hpp"
-unsigned long CRC32Value(int i)
-{
-    int j;
-    unsigned long ulCRC;
-    ulCRC = i;
-    for (j = 8; j > 0; j--)
-    {
-        if (ulCRC & 1)
-            ulCRC = (ulCRC >> 1) ^ 0xEDB88320L;
-        else
-            ulCRC >>= 1;
-    }
-    return ulCRC;
-}
 
-unsigned long CalculateBlockCRC32(unsigned long ulCount, /* Number of bytes in the data block */
-    const unsigned char* ucBuffer) /* Data block */
-{
-    unsigned long ulTemp1;
-    unsigned long ulTemp2;
-    unsigned long ulCRC = 0;
-    while (ulCount-- != 0)
-    {
-        ulTemp1 = (ulCRC >> 8) & 0x00FFFFFFL;
-        ulTemp2 = CRC32Value(((int)ulCRC ^ *ucBuffer++) & 0xff);
-        ulCRC = ulTemp1 ^ ulTemp2;
-    }
-    return(ulCRC);
-}
+#pragma pack(push, 1)
 struct OEM4 {
     double northing;
     double easting;
     double height;
-    uint32_t _garbage1;
-    uint8_t _garbage2;
-    uint32_t crc;
 };
+#pragma pack(pop)
 
-static bool checkCRC32(const unsigned char* data, int n);
-static void printGPSData(OEM4 oem4, unsigned int crc);
+static uint32_t getCRC32(const unsigned char* data, int n);
+static void printGPSData(OEM4 oem4, uint32_t crc);
 
 int main(int argc, char** argv) {
     // Create file mapping object for this process
@@ -77,17 +48,18 @@ int main(int argc, char** argv) {
             unsigned char* buffer = (unsigned char*)(client.tcpReceive());
             unsigned char headerLength = *(buffer + 3);
 
+            // Get CRC value
+            uint32_t crc = *(uint32_t*)(buffer + headerLength + 80);
+
             // Consider non-corrupt GPS data
-            if (checkCRC32(buffer, 108) == false) {
+            if (getCRC32(buffer, 108) == crc) {
 
                 // Process GPS data and store in shared memory
                 OEM4* oem4 = ((OEM4*)map.getBaseAddress());
-                *oem4 = *(OEM4*)((char*)buffer + headerLength + 16);
-                std::cout << oem4->crc << std::endl;
-                uint32_t crc = (uint32_t)(buffer + headerLength + 80);
+                *oem4 = *(OEM4*)(buffer + headerLength + 16);
 
                 // Print GPS data
-                // printGPSData(*oem4, crc);
+                printGPSData(*oem4, crc);
             }
 
             // Set heartbeat
@@ -106,29 +78,26 @@ int main(int argc, char** argv) {
 /**
  * CRC-32 algorithm
  */
-static bool checkCRC32(const unsigned char* data, int n) {
+static uint32_t getCRC32(const unsigned char* data, int n) {
+    uint32_t crc32 = 0;
+    while (n-- != 0) {
+        uint32_t temp = ((int)crc32 ^ *(data++)) & 0xFF;
 
-    uint32_t crc32 = 0xFFFFFFFF;
-
-    for (int i = 0; i < n; i++) {
-        char ch = data[i];
-
-        for (int j = 0; j < 8; j++) {
-            crc32 >>= 1;
-            if ((ch ^ crc32) & 1)
-                crc32 ^= 0xEDB88320;
-            ch >>= 1;
+        for (int j = 8; j > 0; j--) {
+            if (temp & 1)
+                temp = (temp >> 1) ^ 0xEDB88320L;
+            else
+                temp >>= 1;
         }
+        crc32 = ((uint32_t)(crc32 >> 8) & 0x00FFFFFFL) ^ temp;
     }
-    std::cout << ~crc32 << " " << (uint32_t)(data + 108) << " " << CalculateBlockCRC32(n, data) << std::endl;
-
-    return ~crc32 == (uint32_t)(data + 108);
+    return crc32;
 }
 
 /**
  * Print GPS data which includes Northing, Easting, Height and CRC
  */
-static void printGPSData(OEM4 oem4, unsigned int crc) {
+static void printGPSData(OEM4 oem4, uint32_t crc) {
     std::cout << "(N: " << oem4.northing << ", E: " << oem4.easting << ", H: " << oem4.height << ")";
-    std::cout << " + CRC: " << crc << std::endl;
+    std::cout << " + " << crc << std::endl;
 }
