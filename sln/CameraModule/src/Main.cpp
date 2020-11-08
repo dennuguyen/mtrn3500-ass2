931d5abbf1d8f65bd2bf49915521785ec6ce2dcc
Dan Nguyen <z5206032@ad.unsw.edu.au>
2020-11-08 23:13:36 +1100

    Connect to server camera

--- sln/CameraModule/src/Main.cpp
+++ sln/CameraModule/src/Main.cpp
@@ -5,6 +5,7 @@
 #include <GL/freeglut.h>
 #include <turbojpeg.h>
 #include <Windows.h>
+#include <zmq.hpp>
 
 #include "Modules.hpp"
 #include "SharedMemory.hpp"
@@ -12,61 +13,50 @@
 #include "Timer.hpp"
 
 #pragma comment(lib, "turbojpeg.lib")
+#pragma comment(lib, "libzmq-v120-mt-gd-4_0_4.lib")
 
 static GLuint texture;
-static tcp::TCPClient client(mod::CAMERA.ip, mod::CAMERA.port, mod::ZID);
+static zmq::context_t context(1);
+static zmq::socket_t subscriber(context, ZMQ_SUB);
+tmr::Timer timer;
+static bool* heartbeat;
 
 static void display();
 static void idle();
 
 int main(int argc, char* argv[]) {
 
-    // Connect TCP client
-    client.tcpConnect();
-
     // Create file mapping object to process management
     sm::FileMappingObject management(mod::MANAGE.name, sm::SIZE);
     management.openFileMapping();
-    bool* heartbeat = (bool*)((char*)management.mappedViewAddr() + mod::CAMERA.heartbeat);
+    heartbeat = (bool*)((char*)management.mappedViewAddr() + mod::CAMERA.heartbeat);
 
-    // Create timer
-    tmr::Timer timer;
+    // Start timer
     timer.time(tmr::TIMEOUT_4S);
+
+    // Socket to talk to server
+    subscriber.connect("tcp://192.168.1.200:26000");
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
 
     // Initialise GL window
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
     glutInitWindowPosition(0, 0);
-    glutInitWindowSize(800, 600);
+    glutInitWindowSize(200, 200);
     glutCreateWindow("UGV Camera");
 
     // Specify GL callbacks
     glutDisplayFunc(display);
     glutIdleFunc(idle);
     glGenTextures(1, &texture);
+
+    // Run main
     glutMainLoop();
-
-    while (!timer.expired()) {
-
-        std::cout << "heartbeat\n";
-
-        if (*heartbeat == false) {
-            *heartbeat = true;
-            timer.time(tmr::TIMEOUT_4S);
-        }
-        Sleep(1000);  // 1000 ms refresh rate
-    }
-
-    // Close TCP client
-    client.tcpClose();
-
-    std::cin.ignore();
 
     return EXIT_SUCCESS;
 }
 
 static void display() {
-    std::cout << "display\n";
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -86,37 +76,47 @@ static void display() {
 }
 
 static void idle() {
-    std::cout << "idle\n";
-    std::string buffer = client.tcpReceive();
-    std::cout << buffer.c_str() << std::endl;
+    zmq::message_t buffer;
+    if (subscriber.recv(&buffer, ZMQ_NOBLOCK)) {
+        unsigned long numBytes = buffer.size();
+        unsigned char* compressed = (unsigned char*)buffer.data();
+        int jpegSubsample = 0, width = 0, height = 0;
 
-    unsigned long numBytes = buffer.size();
-    unsigned char* compressed = (unsigned char*)buffer.data();
-    int jpegSubsample = 0, width = 0, height = 0;
+        tjhandle decompressor = tjInitDecompress();
+        if (tjDecompressHeader2(decompressor, compressed, numBytes, &width, &height, &jpegSubsample) == -1) {
+            std::cerr << "ERROR: Could not decompress header" << std::endl;
+            glutLeaveMainLoop();
+        }
 
-    tjhandle decompressor = tjInitDecompress();
-    if (tjDecompressHeader2(decompressor, compressed, numBytes, &width, &height, &jpegSubsample) == -1) {
-        std::cerr << "ERROR: Could not decompress header" << std::endl;
-        //glutLeaveMainLoop();
+        unsigned char* decompressed = new unsigned char[width * height * 3];
+
+        if (tjDecompress2(decompressor, compressed, numBytes, decompressed, width, 0, height, TJPF_RGB, TJFLAG_FASTDCT) == -1) {
+            std::cerr << "ERROR: Could not decompress image" << std::endl;
+            glutLeaveMainLoop();
+        }
+
+        if (tjDestroy(decompressor) == -1) {
+            std::cerr << "ERROR: Could not destroy turbojpeg handle" << std::endl;
+            glutLeaveMainLoop();
+        }
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, decompressed);
+        delete[] decompressed;
     }
 
-    unsigned char* decompressed = new unsigned char[width * height * 3];
-    
-    if (tjDecompress2(decompressor, compressed, numBytes, decompressed, width, 0, height, TJPF_RGB, TJFLAG_FASTDCT) == -1) {
-        std::cerr << "ERROR: Could not decompress image" << std::endl;
-        //glutLeaveMainLoop();
+    display();
+
+    if (timer.expired()) {
+        glutLeaveMainLoop();
     }
-    
-    if (tjDestroy(decompressor) == -1) {
-        std::cerr << "ERROR: Could not destroy turbojpeg handle" << std::endl;
-        //glutLeaveMainLoop();
+
+    if (*heartbeat == false) {
+        *heartbeat = true;
+        timer.time(tmr::TIMEOUT_4S);
     }
-    
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, decompressed);
-    delete[] decompressed;
 }
