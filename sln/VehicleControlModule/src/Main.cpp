2390adb85df60b5588a2c062abb6b785ff5a504f
Dan Nguyen <z5206032@ad.unsw.edu.au>
2020-11-17 02:42:23 +1100

    Write teleop main structure

--- sln/VehicleControlModule/src/Main.cpp
+++ sln/VehicleControlModule/src/Main.cpp
@@ -36,25 +36,17 @@ int main(int argc, char* argv[]) {
     while (!timer.expired()) {
         if (*heartbeat == false) {
 
-            double steer = 20.0;
-            double speed = 0.3;
-            bool flag = 1;
-            
-            // Get teleop values
-            // auto [steer, speed, flag] = teleop.get();
-
-            // Send teleop
-            std::cout << "Sending command" << std::endl;
+            // Get teleop command
             std::stringstream command;
+            auto [steer, speed, flag] = teleopInput();
             command << "# " << steer << " " << speed << " " << flag << " #";
+
+            // Send teleop command
             std::cout << client.tcpSend(command.str()) << std::endl;
 
-            // Print sent message
+            // Print teleop command
             printCommand(command.str());
 
-            // Give server time to prepare data
-            Sleep(500);
-            
             // Set heartbeat
             *heartbeat = true;
             timer.time(tmr::TIMEOUT_2S);
@@ -70,44 +62,30 @@ int main(int argc, char* argv[]) {
 
 static std::tuple<double, double, bool> teleopInput() {
 
-    double steer, speed;
-    bool flag;
+    double steer = 0.0, speed = 0.0;
+    bool flag = 0;
 
-    // Read pressed keys
     if (KeyManager::get().isAsciiKeyPressed('w')) {
         speed = 1.0;
-    } else {
-        speed = 0.0;
     }
 
     if (KeyManager::get().isAsciiKeyPressed('a')) {
         speed = -1.0;
-    } else {
-        speed = 0.0;
     }
 
     if (KeyManager::get().isAsciiKeyPressed('s')) {
         steer = 40.0;
-    } else {
-        steer = 0.0;
     }
 
     if (KeyManager::get().isAsciiKeyPressed('d')) {
         steer = -40.0;
-    } else {
-        steer = 0.0;
     }
-
-    // Get teleop values
     
-    std::cout << "<steer> <speed> <flag> ";
-    std::cin >> steer >> speed >> flag;
-
     // Limit teleop values
     steer = limit(steer, -40, 40);
     speed = limit(speed, -1, 1);
 
-    return {steer, speed, flag};
+    return { steer, speed, flag };
 }
 
 /**
